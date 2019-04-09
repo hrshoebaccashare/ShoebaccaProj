93bc336fb18682e12861d67071ab8c10e0c7837b
Gabriel Michaud <gabriel@velixo.com>
2019-04-09 09:56:03 -0400

    Updated dropship fees item handling

--- POOrderExt.cs
+++ POOrderExt.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Linq;
 using PX.Data;
+using PX.Data.BQL;
+using PX.Data.BQL.Fluent;
 using PX.Objects.AP;
 using PX.Objects.AR;
 using PX.Objects.CR;
@@ -15,153 +18,133 @@ namespace ShoebaccaProj
 {
     public class POOrderEntryExt : PXGraphExtension<POOrderEntry>
     {
-        public PXSelect<POLine> POline;
-
-        protected virtual void POOrder_RowPersisting(PXCache sender, PXRowPersistingEventArgs e)
+        public delegate void PersistDelegate();
+        [PXOverride]
+        public void Persist(PersistDelegate baseMethod)
         {
-            POOrder row = e.Row as POOrder;
-            if (row == null) return;
-            decimal value = 0;
-            decimal lineTotal = 0;
-            POLine newLine;
-
-            if (e.Operation != PXDBOperation.Delete)
+            if (Base.Document.Current != null && Base.Document.Cache.GetStatus(Base.Document.Current) == PXEntryStatus.Inserted && Base.Document.Current.OrderType == POOrderType.DropShip)
             {
-                if (row.OrderType == "DP")    // KN - CN/JN 2Dec16, Added condition, So that The newline is only added for DropShip type PO's
+                var row = Base.Document.Current;
+
+                //Copy ship via from SO
+                SOOrder salesOrder = SelectFrom<SOOrder>
+                   .Where<SOOrder.orderType.IsEqual<@P.AsString>
+                       .And<SOOrder.orderNbr.IsEqual<@P.AsString>>>
+                   .View
+                   .ReadOnly
+                   .Select(Base, row.SOOrderType, row.SOOrderNbr);
+
+                if(salesOrder != null)
                 {
-                    if (row.SOOrderNbr != null && row.SOOrderType != null && row.OrderType == "DP" && e.Operation == PXDBOperation.Insert)
-                    {
-                        //Copy ShipVia from Sales Order to Purchase Order
-                        SOOrder SOord = PXSelect<SOOrder,
-                                                                        Where<SOOrder.orderNbr, Equal<Required<SOOrder.orderNbr>>,
-                                                                        And<SOOrder.orderType, Equal<Required<SOOrder.orderType>>>>>
-                                                                        .Select(Base, row.SOOrderNbr, row.SOOrderType);
-                        //Commented by bhavya to change the shipvia at PO level on 2018/26/10
-                        //  if (SOord != null) row.ShipVia = SOord.ShipVia;
-                        // revoked the commented line and added the  appended the condition check  e.Operation == PXDBOperation.Insert at the top by deebhan on 18th Jan 2019
-                        if (SOord != null) row.ShipVia = SOord.ShipVia;
-                    }
+                    row.ShipVia = salesOrder.ShipVia;
+                }
 
-                    //Get the Inventory Item for dropship fees to use for defaulting values
-                    InventoryItem item = PXSelect<InventoryItem,
-                                                                                Where<InventoryItem.inventoryCD, Equal<Required<InventoryItem.inventoryCD>>>>
-                                                                                .Select(Base, "DROPSHIP FEE");
+                //Get the Inventory Item for dropship fees to use for defaulting values
+                InventoryItem dropshipFeeItem = SelectFrom<InventoryItem>
+                    .Where<InventoryItem.inventoryCD.IsEqual<@P.AsString>>
+                    .View
+                    .ReadOnly
+                    .Select(Base, "DROPSHIP FEE");
 
-                    //If item is null, just bail because you can't ad items anyway
-                    if (item == null)
-                    {
-                        POline.Ask("Please setup a 'DROPSHIP FEE' Inventory Item for Dropship Fee items creation.", MessageButtons.OK);
-                        return;
-                    }
+                //If item is null, just bail because you can't ad items anyway
+                if (dropshipFeeItem == null)
+                {
+                    throw new PXException(ShoebaccaProj.Messages.MissingDropshipFeeItem);
+                }
 
-                    BAccount account = PXSelect<BAccount, Where<BAccount.type, Equal<Required<BAccount.type>>, And<BAccount.bAccountID, Equal<Required<BAccount.bAccountID>>>>>.Select(Base, BAccountType.VendorType, row.VendorID);
+                var fees = GetVendorDropshipFees(row.VendorID.Value);
+                if(fees.DiscountPercentage != 0 && !DropshipFeesItemExistOnInvoice(dropshipFeeItem.InventoryID.Value, Messages.DropShipDiscountPercentage))
+                {
+                    decimal totalCost = Base.Transactions.Select().Cast<POLine>().Where(line => line.InventoryID != dropshipFeeItem.InventoryID).Sum(line => line.ExtCost.Value);
+                    decimal discount = (fees.DiscountPercentage / 100) * totalCost;
+                    AddDropshipFees(dropshipFeeItem.InventoryID.Value, Messages.DropShipOrderHandlingFee, 1, -discount);
+                }
 
-                    //Add Drop ship PO Line Items based on vendor attributes
-                    foreach (PXResult<CSAnswers, CSAttribute> attr in PXSelectJoin<CSAnswers,
-                                                                                                    InnerJoin<CSAttribute, On<CSAttribute.attributeID, Equal<CSAnswers.attributeID>>>,
-                                                                                                    Where<CSAnswers.refNoteID, Equal<Required<CSAnswers.refNoteID>>,
-                                                                                                    And<Where<CSAnswers.attributeID, Equal<Required<CSAnswers.attributeID>>,
-                                                                                                                        Or<CSAnswers.attributeID, Equal<Required<CSAnswers.attributeID>>,
-                                                                                                                        Or<CSAnswers.attributeID, Equal<Required<CSAnswers.attributeID>>>>>>>>
-                                                                                                    .Select(Base, account.NoteID, "DSORDHANDL", "DSITMHANDL", "DSDISCTPCT"))
-                    {
-                        CSAnswers ans = attr;
-                        CSAttribute atr = attr;
+                if(fees.ItemHandlingFee != 0 && !DropshipFeesItemExistOnInvoice(dropshipFeeItem.InventoryID.Value, Messages.DropShipItemHandlingFee))
+                {
+                    int lineCount = Base.Transactions.Select().Cast<POLine>().Where(line => line.InventoryID != dropshipFeeItem.InventoryID).Count();
+                    AddDropshipFees(dropshipFeeItem.InventoryID.Value, Messages.DropShipOrderHandlingFee, lineCount, fees.ItemHandlingFee);
+                }
 
-                        //See if this POLine already exists
-                        POLine line = PXSelect<POLine,
-                                                                        Where<POLine.orderNbr, Equal<Current<POOrder.orderNbr>>,
-                                                                        And<POLine.inventoryID, Equal<Required<POLine.inventoryID>>,
-                                                                            And<POLine.tranDesc, Equal<Required<POLine.tranDesc>>>>>>
-                                                                        .Select(Base, item.InventoryID, atr.Description);
-                        if (line == null)
-                        {
-                            newLine = POline.Insert();
-                            newLine.OrderNbr = row.OrderNbr;
-                            newLine.OrderType = row.OrderType;
-                            newLine.InventoryID = item.InventoryID;
-                            newLine.TranDesc = atr.Description;
-                            newLine.OrderQty = 1;
-                            newLine.UOM = item.BaseUnit;
-                            newLine.ExpenseAcctID = item.COGSAcctID;
-                            newLine.ExpenseSubID = item.COGSSubID;
-                          
-                          
-                            line = newLine;
-                        }
-                        switch (ans.AttributeID)
-                        {
-                            case "DSORDHANDL":
-                                if (decimal.TryParse(ans.Value, out value))
-                                {
-                                    line.UnitCost = value;
-                                    line.CuryUnitCost = value;
-                                    line.LineAmt = value;
-                                    line.CuryLineAmt = value;
-                                }
-                                break;
-                            case "DSDISCTPCT":
-                                if (decimal.TryParse(ans.Value, out value))
-                                {
-
-                                     lineTotal = 0m;
-                                    foreach (POLine lineItem in PXSelect<POLine,
-                                                                                                                Where<POLine.orderNbr, Equal<Current<POOrder.orderNbr>>,
-                                                                                                                And<POLine.inventoryID, NotEqual<Required<POLine.inventoryID>>>>>
-                                                                                                                .Select(Base, item.InventoryID))
-                                    {
-                                        lineTotal += (lineItem.ExtCost ?? 0);
-                                    }
-                                    value = -(value / 100) * lineTotal;
-                                    line.UnitCost = value;
-                                    line.CuryUnitCost = value;
-                                    line.LineAmt = value;
-                                    line.CuryLineAmt = value;
-                                }
-                                break;
-                            case "DSITMHANDL":
-
-                                //if (decimal.TryParse(ans.Value, out value))
-                                //{
-                                //    decimal lineCount = PXSelect<POLine,
-                                //                                                            Where<POLine.orderNbr, Equal<Current<POOrder.orderNbr>>,
-                                //                                                            And<POLine.inventoryID, NotEqual<Required<POLine.inventoryID>>>>>
-                                //                                                            .Select(Base, item.InventoryID).Count;
-                                //    line.OrderQty = lineCount;
-                                //    line.UnitCost = value;
-                                //    line.CuryUnitCost = value;
-                                //    value *= lineCount;
-                                //    line.LineAmt = value;
-                                //    line.CuryLineAmt = value;
-                                //}
-
-                                // Commented  above logic and modified the below  one for the wrike ticket ID 277235649  by Deebhan on 28th Oct 2018 
-                                if (decimal.TryParse(ans.Value, out value))
-                                {
-                                    decimal? lineqty = 0m;
-                                    foreach (POLine lineItem in PXSelect<POLine,
-                                                                                                              Where<POLine.orderNbr, Equal<Current<POOrder.orderNbr>>,
-                                                                                                              And<POLine.inventoryID, NotEqual<Required<POLine.inventoryID>>>>>
-                                                                                                              .Select(Base, item.InventoryID))
-                                    {
-                                        lineqty += (lineItem.OrderQty ?? 0);
-                                    }
-                                    line.OrderQty = lineqty;
-                                    line.ManualPrice = true;
-                                    line.UnitCost = Convert.ToDecimal(ans.Value);
-                                    line.CuryUnitCost = Convert.ToDecimal(ans.Value);
-                                    value *= Convert.ToDecimal(lineqty);
-                                    line.LineAmt = Convert.ToDecimal(ans.Value) * lineqty;
-                                    line.CuryLineAmt = Convert.ToDecimal(ans.Value) * lineqty;
-                                }
-                                break;
-                            default:
-                                break;
-                        }
-                        POline.Update(line);
-                    }
+                if (fees.OrderHandlingFee != 0 && !DropshipFeesItemExistOnInvoice(dropshipFeeItem.InventoryID.Value, Messages.DropShipOrderHandlingFee))
+                {
+                    AddDropshipFees(dropshipFeeItem.InventoryID.Value, Messages.DropShipOrderHandlingFee, 1, fees.OrderHandlingFee);
                 }
             }
+            
+            baseMethod();
         }
+
+        private VendorDropshipFees GetVendorDropshipFees(int vendorID)
+        {
+            var fees = new VendorDropshipFees();
+            var vendorAttributes = SelectFrom<CSAnswers>
+                  .InnerJoin<Vendor>
+                      .On<CSAnswers.refNoteID.IsEqual<Vendor.noteID>>
+                  .Where<Vendor.bAccountID.IsEqual<@P.AsInt>>
+                  .View
+                  .ReadOnly
+                  .Select(Base, vendorID);
+
+            foreach(CSAnswers attr in vendorAttributes)
+            {
+                if (String.IsNullOrEmpty(attr.Value)) continue;
+
+                switch(attr.AttributeID)
+                {
+                    case "DSDISCTPCT":
+                        if (!decimal.TryParse(attr.Value, out fees.DiscountPercentage))
+                        {
+                            throw new PXException(ShoebaccaProj.Messages.FailedToParseVendorAttribute, attr.AttributeID, vendorID, attr.Value);
+                        }
+                        break;
+                    case "DSITMHANDL":
+                        if (!decimal.TryParse(attr.Value, out fees.ItemHandlingFee))
+                        {
+                            throw new PXException(ShoebaccaProj.Messages.FailedToParseVendorAttribute, attr.AttributeID, vendorID, attr.Value);
+                        }
+                        break;
+                    case "DSORDHANDL":
+                        if(!decimal.TryParse(attr.Value, out fees.OrderHandlingFee))
+                        {
+                            throw new PXException(ShoebaccaProj.Messages.FailedToParseVendorAttribute, attr.AttributeID, vendorID, attr.Value);
+                        }
+                        break;
+                }
+            }
+
+            return fees;
+        }
+
+        private bool DropshipFeesItemExistOnInvoice(int inventoryID, string description)
+        {
+            foreach(POLine line in Base.Transactions.Select())
+            {
+                if(line.InventoryID == inventoryID && line.TranDesc == description)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void AddDropshipFees(int inventoryID, string description, decimal? quantity, decimal? unitCost)
+        {
+            var newLine = (POLine) Base.Transactions.Insert();
+            newLine.InventoryID = inventoryID;
+            newLine.TranDesc = description;
+            newLine.ManualPrice = true;
+            newLine.OrderQty = quantity;
+            newLine.CuryUnitCost = unitCost;
+            Base.Transactions.Update(newLine);
+        }
+    }
+
+    public class VendorDropshipFees
+    {
+        public decimal OrderHandlingFee;
+        public decimal ItemHandlingFee;
+        public decimal DiscountPercentage;
     }
 }
